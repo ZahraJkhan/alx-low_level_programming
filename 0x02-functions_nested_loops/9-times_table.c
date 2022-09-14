ec73c1e4624c5decb689cd98d841c7aa93787970
ZahraJkhan <zahrajid@gmail.com>
2022-09-14 08:09:53 -0700

    function that prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,17 +13,25 @@ void times_table(void)
 
 		for (mult = '1'; mult <= 9; mult++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
 			prod = num * mult;
 
-			if (prod <= 9)
-				_putchar(' ');
+			if (mult == 0)
+				_putchar('0' + prod);
+			else if (prod < 10)
+			{
+				_putchar(',');
+				_putchar('0' + prod);
+			}
 			else
-				_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
+				_putchar('0' + prod / 10);
+			_putchar('0' + prod % 10);
 		}
-		_putchar('\n');
+		if (mult < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
+	_putchar('\n');
+}
 }
