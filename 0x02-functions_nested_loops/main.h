433c92bcf66a57ac049b7f5817c24514195dbdb9
ZahraJkhan <zahrajid@gmail.com>
2022-09-14 02:23:07 -0700

    program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,5 @@
-/**
- * main - Header file containing declaration of all functions
- */
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -15,3 +14,5 @@ int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
 void putformat(int);
+
+#endif
