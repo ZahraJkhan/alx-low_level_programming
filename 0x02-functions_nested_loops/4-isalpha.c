11859b97e1e16e48fe0b343f5361d9730b643fba
ZahraJkhan <zahrajid@gmail.com>
2022-09-14 03:55:09 -0700

    function that checks for alphabetic character.

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+nclude "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ *
+ * Return: 1 if the character is letter, or uppercase else 0
+ */
+int _isalpha(int c);
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
