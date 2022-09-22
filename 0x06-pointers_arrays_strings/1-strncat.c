45be8cf41f778fbb87b0a2aa85ad1ec8780c827e
ZahraJkhan <zahrajid@gmail.com>
2022-09-22 05:44:02 -0700

    function that concatenates two strings using atmost n bytes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenates two strings using input no frm src
+ * _strncat - Concatenates two strings using input no frm src
  * @dest: The string to be appended upon.
  * @src: The string to be appended to dest
  * @n: The number of bytes from src to be appended to dest.
