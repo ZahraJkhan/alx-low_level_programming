d9cb90c28d78ea7e6f6c47b15840d0b1d77e3e53
ZahraJkhan <zahrajid@gmail.com>
2022-09-30 01:47:41 -0700

    prog that prints all arg it receives incl first one

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prog prints all arg it receives incl 1st one
+ * @argc: no of arg passed to func
+ * @argv: arg vector pointer to string
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0, i < argc, i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
